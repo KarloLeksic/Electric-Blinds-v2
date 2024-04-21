b6d56f36e4fb4cded6f81922b9e7c813e0ed7e3d
Karlo <lekxka@gmail.com>
2024-04-21 15:11:37 +0200

    adding formatting options and formatting the code

--- Electric-Blinds-v2.ino
+++ Electric-Blinds-v2.ino
@@ -1,41 +1,49 @@
 TaskHandle_t task_loop1;
 SemaphoreHandle_t lock = NULL;
 
-void lock_lock() {
-  while ((lock == NULL) || (xSemaphoreTake(lock, (TickType_t)0) == pdFALSE)) {
-    delay(1);
-  }
+void lock_lock()
+{
+    while ((lock == NULL) || (xSemaphoreTake(lock, (TickType_t)0) == pdFALSE))
+    {
+        delay(1);
+    }
 }
 
-void lock_unlock() {
-  xSemaphoreGive(lock);
+void lock_unlock()
+{
+    xSemaphoreGive(lock);
 }
 
-void esploop1(void* pvParameters) {
-  for (;;)
-    loop1();
+void esploop1(void *pvParameters)
+{
+    for (;;)
+        loop1();
 }
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+    Serial.begin(115200);
 
-  while (!Serial) {
-    delay(1); 
-  }
+    while (!Serial)
+    {
+        delay(1);
+    }
 
-  xTaskCreatePinnedToCore(esploop1, "loop1", 10000, NULL, 1, &task_loop1, !ARDUINO_RUNNING_CORE);
+    xTaskCreatePinnedToCore(esploop1, "loop1", 10000, NULL, 1, &task_loop1, !ARDUINO_RUNNING_CORE);
 
-  lock = xSemaphoreCreateMutex();
+    lock = xSemaphoreCreateMutex();
 }
 
-void loop() {
-  Serial.printf("loop%d()\n", xPortGetCoreID());
+void loop()
+{
+    Serial.printf("loop%d()\n", xPortGetCoreID());
 
-  delay(1000);
+    delay(1000);
 }
 
-void loop1() {
-  Serial.printf("loop%d()\n", xPortGetCoreID());
+void loop1()
+{
+    Serial.printf("loop%d()\n", xPortGetCoreID());
 
-  delay(1000);
+    delay(1000);
 }
